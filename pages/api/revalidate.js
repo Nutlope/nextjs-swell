337099980dafce3e4981235a7734002010e8e407
nutlope <he55@drexel.edu>
2022-04-14 15:06:30 -0400

    change to on-demand ISR

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -3,13 +3,13 @@ export default async function handler(req, res) {
     return res.status(401).json({ message: 'Invalid token' })
   }
   try {
-    // Regenerate our index route showing the images
-    console.log('req is ', req)
+    // Regenerate our index and product routes
+    const productName = req.body.data.name
+    const productSlug = productName.replace(/\s/g, '-').toLowerCase()
     await res.unstable_revalidate('/')
+    await res.unstable_revalidate(`/${productSlug}`)
     return res.json({ revalidated: true })
   } catch (err) {
-    // If there was an error, Next.js will continue
-    // to show the last successfully generated page
     return res.status(500).send('Error revalidating')
   }
 }
