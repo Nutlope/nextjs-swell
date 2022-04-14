76644e9a5607afc4fd076f3c96e91542220af8c5
nutlope <he55@drexel.edu>
2022-04-14 15:36:24 -0400

    took out JSON

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -3,12 +3,11 @@ export default async function handler(req, res) {
     return res.status(401).json({ message: 'Invalid token' })
   }
 
-  const { data } = JSON.parse(req.body)
-  console.log('data is: ', data)
   // Regenerate our index and product routes
   try {
+    console.log('req is: ', req.body)
     await res.unstable_revalidate('/')
-    await res.unstable_revalidate(`/products/${data.id}`)
+    await res.unstable_revalidate(`/products/${req.body.data.id}`)
     return res.status(200).json({ revalidated: true })
   } catch (err) {
     return res.status(500).send('Error revalidating')
