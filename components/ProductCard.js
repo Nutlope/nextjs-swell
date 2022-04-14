3976f125d520d4c7485336eb5545d1efdfd6cb03
nutlope <he55@drexel.edu>
2022-04-14 15:17:59 -0400

    added per page revalidation

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -8,7 +8,7 @@ export default function ProductCard({ product }) {
     return classes.filter(Boolean).join(' ')
   }
   return (
-    <Link href={`/products/${product.slug}`}>
+    <Link href={`/products/${product.id}`}>
       <a className="group">
         <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
           <Image
--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -2,13 +2,13 @@ export default async function handler(req, res) {
   if (req.query.secret !== process.env.REVALIDATE_SECRET) {
     return res.status(401).json({ message: 'Invalid token' })
   }
+
+  const { data } = JSON.parse(req.body)
+  // Regenerate our index and product routes
   try {
-    // Regenerate our index and product routes
-    const productName = req.body.data.name
-    const productSlug = productName.replace(/\s/g, '-').toLowerCase()
     await res.unstable_revalidate('/')
-    await res.unstable_revalidate(`/${productSlug}`)
-    return res.json({ revalidated: true })
+    await res.unstable_revalidate(`/${data.id}`)
+    return res.status(200).json({ revalidated: true })
   } catch (err) {
     return res.status(500).send('Error revalidating')
   }
